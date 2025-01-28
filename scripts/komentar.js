d0f4c0cf11db6078dec252f0ad90b095ae69bce1
Faldiansyah <faldiansyahk@gmail.com>
2025-01-28 17:28:56 +0700

    writing a comment

---
+++ scripts/komentar.js
@@ -0,0 +1,9 @@
+
+// Ini komentar satu baris
+document.writeln("<p>Hello komentar</p>")
+
+/*
+Ini komentar lebih dari satu baris
+bebas mau nulis apa aja
+*/
+document.writeln("<p>Hello komentar lebih dari satu baris</p>")
