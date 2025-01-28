b1026fbdabf4bd099874ce4b00d3a8164779f8c5
Faldiansyah <faldiansyahk@gmail.com>
2025-01-28 17:51:03 +0700

    with addition operator

--- scripts/tipe-data-string.js
+++ scripts/tipe-data-string.js
@@ -1,3 +1,8 @@
 document.writeln("Ini tipe data string")
 document.writeln("<br>")
 document.writeln('Bisa juga pake petik satu')
+document.writeln("<br>")
+
+document.writeln("Nama : " + "Faldi" + " Ahmad")
+document.writeln("<br>")
+document.writeln("Channel : " + "Faldi")
