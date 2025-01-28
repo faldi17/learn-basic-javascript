6cb0250f6f2c3b6f54823388dc0d4275b8b684d5
Faldiansyah <faldiansyahk@gmail.com>
2025-01-28 18:00:45 +0700

    escape sequence

---
+++ scripts/escape-sequence.js
@@ -0,0 +1,8 @@
+
+
+document.writeln("<textarea cols='100' rows='10'>")
+document.write("Faldi \nPujo \nAlung\n")
+document.write("\\Home\\faldi\n")
+document.write("\"Programmer Generasi Emas\"\n")
+document.write('\'Belajar Javascript\'\n')
+document.writeln("</textarea>")
