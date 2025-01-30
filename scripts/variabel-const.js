c77be7ac54901d72edd10aec5f792cf308b3254e
Faldiansyah <faldiansyahk@gmail.com>
2025-01-30 07:22:03 +0700

    using const variables

---
+++ scripts/variabel-const.js
@@ -0,0 +1,4 @@
+const apps = "Pengen Belajar JavaScript"
+// apps = "Gak Mau Belajar JavaScript"
+// line dua diatas error karna const sifatnya ga bisa diubah, jadi kita berikan comment
+document.writeln(apps) 
