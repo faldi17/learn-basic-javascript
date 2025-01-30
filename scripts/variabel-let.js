9c96ca5c65462c8587e0a098344de6aaf6f32462
Faldiansyah <faldiansyahk@gmail.com>
2025-01-30 07:17:22 +0700

    using let variables

---
+++ scripts/variabel-let.js
@@ -0,0 +1,18 @@
+let fullName = "Jabir Bin Hayyan";
+let firstName = "Jabir";
+let middleName = "Bin";
+let lastName = "Hayyan";
+
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+
+fullName = "Ibnu Sina";
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
