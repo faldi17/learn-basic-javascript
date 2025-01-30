f21349ed07465e9657dfcfa014bffbad7e86f96f
Faldiansyah <faldiansyahk@gmail.com>
2025-01-30 07:13:05 +0700

    how to make variable

---
+++ scripts/variabel.js
@@ -0,0 +1,18 @@
+var fullName = "Jabir Bin Hayyan"
+var firstName = "Jabir"
+var middleName = "Bin"
+var lastName = "Hayyan"
+
+document.writeln(fullName)
+document.writeln("<br>")
+document.writeln(fullName)
+document.writeln("<br>")
+document.writeln(fullName)
+document.writeln("<br>")
+document.writeln(fullName)
+document.writeln("<br>")
+
+fullName = "Ibnu Sina"
+document.writeln(fullName)
+document.writeln("<br>")
+document.writeln(fullName)
