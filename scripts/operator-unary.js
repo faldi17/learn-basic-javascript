2c249d7ea249e98c9483be9c098588b125921098
Faldiansyah <faldiansyahk@gmail.com>
2025-01-30 14:45:41 +0700

    unary operators

---
+++ scripts/operator-unary.js
@@ -0,0 +1,15 @@
+// positive
+let result = +1
+document.writeln("<p>" + result + "</p>")
+
+// increment
+result++ // result = result + 1 or result += 1
+document.writeln("<p>" + result + "</p>")
+
+// decrement
+result-- // result = result - 1 or result -= 1
+document.writeln("<p>" + result + "</p>")
+
+// negative
+result = -result // -1
+document.writeln("<p>" + result + "</p>")
