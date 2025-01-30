e6ea25c9af46af99f3ebf5005eb16fb71fc3ebdb
Faldiansyah <faldiansyahk@gmail.com>
2025-01-30 15:15:05 +0700

    comparison operators

---
+++ scripts/operator-perbandingan.js
@@ -0,0 +1,31 @@
+// greater than
+let result = 5 > 10
+document.writeln("<p>" + result + "</p>")
+
+// less than
+result = 5 < 10
+document.writeln("<p>" + result + "</p>")
+
+// greater than or equal to
+result = 5 >= 10
+document.writeln("<p>" + result + "</p>")
+
+// less than or equal to
+result = 5 <= 10
+document.writeln("<p>" + result + "</p>")
+
+// equality operator
+result = 5 == "5"
+document.writeln("<p>" + result + "</p>")
+
+// strict equality operator
+result = 5 === "5";
+document.writeln("<p>" + result + "</p>")
+
+// inequality operator
+result = 5 != "5"
+document.writeln("<p>" + result + "</p>")
+
+// strict inequality operator
+result = 5 !== "5"
+document.writeln("<p>" + result + "</p>")
