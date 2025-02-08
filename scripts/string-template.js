93c45c3a08d704860cde7c9fb6ae66be230c5069
Faldiansyah <faldiansyahk@gmail.com>
2025-02-08 14:23:07 +0700

    expression str temp

--- scripts/string-template.js
+++ scripts/string-template.js
@@ -4,3 +4,7 @@ const lastName = "Fadil";
 
 const myName = `My name is ${firstName} ${middleName} ${lastName}`;
 console.info(myName);
+
+const nilai = 90;
+const lulus = `I got a score of ${nilai}, and my passing was declared ${nilai > 75}`;
+console.info(lulus);
