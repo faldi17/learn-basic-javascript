48ffc83f51494513f347a056c030ded844317dab
Faldiansyah <faldiansyahk@gmail.com>
2025-02-08 14:28:16 +0700

    multiline string

--- scripts/string-template.js
+++ scripts/string-template.js
@@ -1,3 +1,4 @@
+// take variable
 const firstName = "Faldi";
 const middleName = "Putra";
 const lastName = "Fadil";
@@ -5,6 +6,17 @@ const lastName = "Fadil";
 const myName = `My name is ${firstName} ${middleName} ${lastName}`;
 console.info(myName);
 
+// expression templates
 const nilai = 90;
 const lulus = `I got a score of ${nilai}, and my passing was declared ${nilai > 75}`;
 console.info(lulus);
+
+// multi-line string
+const bio = `My name is ${firstName} ${middleName} ${lastName}.
+I'm a software engineer, and I'm currently working as a front-end developer.
+I'm passionate about JavaScript and its ecosystem.`;
+
+document.writeln("<pre>");
+document.writeln(bio);
+document.writeln("</pre>");
+console.info(bio);
